6020d27032ab075322f66acade3b351b91066825
Rui Carmo <rui.carmo@gmail.com>
2013-12-03 11:41:42 +0000

    Split single snapshot task

--- tasks/workers.py
+++ tasks/workers.py
@@ -25,21 +25,24 @@ log = logging.getLogger()
 r = Redis(settings.celery.broker_url)
 
 @task(retries=3)
-def snapshot(url, uuid = None):
+def snapshot_set(url, ttl=0, uuid = None):
     if not uuid:
         uuid = str(uuid5("url", uri))
 
     result = []
     for s in settings.phantom.sizes:
         width, height = s.split('x')
-        result_filename = uuid_path("%s.jpg" % s, settings.store.images, uuid)
-        #TODO: check if file already exists, is valid and whether or not it needs updating
-
-        ih, image_filename = tempfile.mkstemp(suffix='.png')
-        subprocess.call([settings.phantom.path,'--ignore-ssl-errors=yes','--ssl-protocol=any',path_for('etc/snap.js'),url,image_filename, width, height, settings.phantom.timeout)
-        os.close(ih)
-        #TODO: validate output file format
-        subprocess.call(settings.magick.args % (settings.magick.path, image_filename, result_filename), shell=True)
-        os.unlink(image_filename)
-        result.append[result_filename]
+        snapshot.delay(url, width, height, ttl, uuid)
     return result
+
+@task(retries=3)
+def snapshot(url, width, height, ttl, uuid):
+    result_filename = uuid_path("%s.jpg" % s, settings.store.images, uuid)
+    #TODO: check if file already exists, is valid and whether or not it needs updating
+    ih, image_filename = tempfile.mkstemp(suffix='.png')
+    subprocess.call([settings.phantom.path,'--ignore-ssl-errors=yes','--ssl-protocol=any',path_for('etc/snap.js'),url,image_filename, width, height, settings.phantom.timeout)
+    os.close(ih)
+    #TODO: validate output file format
+    subprocess.call(settings.magick.args % (settings.magick.path, image_filename, result_filename), shell=True)
+    os.unlink(image_filename)
+    return result_filename
